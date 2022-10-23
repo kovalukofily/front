4644c22eadef9caf1d17f445c662e0ca90f1e050
hitrch <vladislawpaliy@gmail.com>
2022-10-23 23:30:40 +0300

    test#3

--- src/app/content/content.service.ts
+++ src/app/content/content.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 @Injectable()
@@ -9,13 +9,6 @@ export class ContentService {
   }
 
   getWeather(): Observable<any> {
-    // const headerDict = {
-    //   Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    // }
-    //
-    // const requestOptions = {
-    //   headers: new HttpHeaders(headerDict),
-    // };
     return this.http.get( 'https://kovalukofily-backend.herokuapp.com/kyiv_weather')
   }
 }
