b58279cbc67eb2a0621e1218c3ef1608e31f263a
hitrch <vladislawpaliy@gmail.com>
2022-10-23 23:13:24 +0300

    test#2

--- src/app/content/content.service.ts
+++ src/app/content/content.service.ts
@@ -9,15 +9,13 @@ export class ContentService {
   }
 
   getWeather(): Observable<any> {
-    const headerDict = {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    }
-
-    const requestOptions = {
-      headers: new HttpHeaders(headerDict),
-    };
-    return this.http.get( 'https://kovalukofily-backend.herokuapp.com/kyiv_weather', requestOptions)
+    // const headerDict = {
+    //   Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    // }
+    //
+    // const requestOptions = {
+    //   headers: new HttpHeaders(headerDict),
+    // };
+    return this.http.get( 'https://kovalukofily-backend.herokuapp.com/kyiv_weather')
   }
 }
